2c96f8a0976664e2f2b79dddf816f00c79569808
ywj3493 <ywj3493@naver.com>
2025-06-12 11:12:39 +0900

    feat: add header component

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -1,3 +1,26 @@
+import { Link, useLocation } from "react-router-dom";
+
 export function Header() {
-  return <header>Header</header>;
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  return (
+    <header className="grid grid-cols-3 justify-between items-center p-6">
+      <h1 className="text-2xl text-black font-bold">CERTICOS BOOKS</h1>
+      <nav className="flex gap-14 text-xl font-medium text-[#3A3A4A] items-center justify-center">
+        <Link
+          className={`${currentPath === "/search" ? "border-b border-cdri-primary" : ""} underline-offset-4`}
+          to="/search"
+        >
+          도서 검색
+        </Link>
+        <Link
+          className={`${currentPath === "/favorites" ? "border-b border-cdri-primary" : ""} underline-offset-4`}
+          to="/favorites"
+        >
+          내가 찜한 책
+        </Link>
+      </nav>
+    </header>
+  );
 }
