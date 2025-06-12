21d9e6ab355153b490a6e08720582e790c8c4a57
ywj3493 <ywj3493@naver.com>
2025-06-12 11:12:23 +0900

    feat: apply react-router-dom comp to app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,21 @@
-import { SearchBooks } from "../pages/books/search-books";
+import { SearchBooks } from "@/pages/books/search-books";
+import { Header } from "@/widgets/header";
+import { FavoriteBooks } from "@/pages/books/favorite-books";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <main className="font-display">
-      <SearchBooks />
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/search" />} />
+          <Route path="/search" element={<SearchBooks />} />
+          <Route path="/favorites" element={<FavoriteBooks />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
